531b24b28b01088cd1dbc376eeb148d366a4406b
Marc LeBlanc <marc.leblanc@sourcegraph.com>
2024-02-12 12:54:22 +0300

    Adding startup script to start docker compose services using docker compose installed inside WSL 2

---
+++ windows/wsl2-startup.sh
@@ -0,0 +1,4 @@
+# Use a setup like this: https://iknowthatnow.com/2022/04/04/start-wsl2-services-on-windows-startup/
+
+# Start docker compose services
+docker compose -f ../docker-compose.yaml up -d
