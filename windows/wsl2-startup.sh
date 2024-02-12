a5807841a317bc842d890759b7cadf3d07566061
Marc LeBlanc <marc.leblanc@sourcegraph.com>
2024-02-12 14:08:22 +0300

    Updating exit code for failed commands

--- windows/wsl2-startup.sh
+++ windows/wsl2-startup.sh
@@ -1,7 +1,22 @@
+#!/bin/bash
 # Use a setup like this: https://iknowthatnow.com/2022/04/04/start-wsl2-services-on-windows-startup/
 
+log_file="./log"
+git_exit_status=""
+docker_compose_exit_status=""
+
 # Git pull latest commits to main
-git pull
+if ! git pull
+then
+    git_exit_status=$?
+    echo "git pull failed, exit code $git_exit_status" >> $log_file
+    exit $git_exit_status
+fi
 
 # Start docker compose services
-docker compose -f ../docker-compose.yaml up -d
+if ! docker compose -f ../docker-compose.yaml up -d
+then
+    docker_compose_exit_status=$?
+    echo "docker compose up failed, exit code $docker_compose_exit_status" >> $log_file
+    exit $docker_compose_exit_status
+fi
