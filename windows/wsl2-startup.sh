349a32b08a3764cd2f549636552c250fe9d2b288
Marc LeBlanc <marc.leblanc@sourcegraph.com>
2024-02-12 13:10:15 +0300

    Adding git pull step to startup script

--- windows/wsl2-startup.sh
+++ windows/wsl2-startup.sh
@@ -1,4 +1,7 @@
 # Use a setup like this: https://iknowthatnow.com/2022/04/04/start-wsl2-services-on-windows-startup/
 
+# Git pull latest commits to main
+git pull
+
 # Start docker compose services
 docker compose -f ../docker-compose.yaml up -d
