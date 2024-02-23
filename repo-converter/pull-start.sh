7b259d9605979b43a6465b0b0b5294ce5ae86d60
Marc LeBlanc <marc.leblanc@sourcegraph.com>
2024-02-23 15:36:44 +0300

    Switching to latest

--- repo-converter/pull-start.sh
+++ repo-converter/pull-start.sh
@@ -4,10 +4,16 @@
 # so that the running container is only x minutes/hours behind the latest version of the docker-compose.yaml file, and the Docker image tagged latest in GitHub packages
 
 # crontab -e
-# */10 * * * * sudo bash /sourcegraph/implementation-bridges/repo-converter/pull-start.sh >> /sourcegraph/implementation-bridges/repo-converter/pull-start.log 2>&1
+# */10 * * * * sudo bash /sourcegraph/implementation-bridges/repo-converter/pull-start.sh
 
-repo_converter_path="/sourcegraph/implementation-bridges/repo-converter"
+sg_root_dir="/sourcegraph"
+log_file="$repo_converter_dir/pull-start.log"
 
-git -C $repo_converter_path pull
+repo_converter_dir="$sg_root_dir/implementation-bridges/repo-converter"
 
-docker compose -f $repo_converter_path/docker-compose.yaml up -d
+date_time=$(date +"%F %T")
+echo "$date_time - Starting $0" >> "$log_file"
+
+command="git -C $repo_converter_dir pull && docker compose -f $repo_converter_dir/docker-compose.yaml up -d"
+
+bash -c "$command" >> "$log_file" 2>&1
